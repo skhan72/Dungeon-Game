9a4b4b5343daa489d2f299f571238fb6bb6b5895
skhan72 <summaiyajalal@gmail.com>
2025-05-12 20:37:12 -0500

    added chambers and doors

--- src/main/java/org/example/Door.java
+++ src/main/java/org/example/Door.java
@@ -1,4 +1,60 @@
 package org.example;
-
+ /**
+  * The Door class represents a door between two chambers in the dungeon.
+  */
 public class Door {
+    private Chamber chamber1;
+    private Chamber chamber2;
+    private Monster monster;
+
+    /**
+     * Creates a door between two chambers and optionally associates a monster with the door.
+     *
+     * @param chamber1 The first chamber connected by the door.
+     * @param chamber2 The second chamber connected by the door.
+     * @param monster The monster guarding the door, or null if the door is unguarded.
+     */
+    public Door(Chamber chamber1, Chamber chamber2, Monster monster) {
+        this.chamber1 = chamber1;
+        this.chamber2 = chamber2;
+        this.monster = monster;
+    }
+
+
+    public Chamber getOtherChamber(Chamber from) {
+        if (from.equals(chamber1)) {
+            return chamber2;
+        } else if (from.equals(chamber2)) {
+            return chamber1;
+        } else {
+            return null;
+        }
+    }
+
+    public boolean hasMonster() {
+        return monster != null && monster.getHealth() > 0;
+    }
+
+    /**
+     * Returns the monster guarding the door, if there is one.
+     *
+     * @return The Monster object guarding the door, or null if the door is unguarded.
+     */
+    public Monster getMonster() {
+        return monster;
+    }
+
+    /**
+     * Checks if the door is currently guarded by a monster.
+     *
+     * @return True if the door is guarded, false otherwise.
+     */
+    public boolean isGuarded() {
+        return monster != null && monster.getHealth() > 0;
+    }
+
+    public void removeMonster() {
+        this.monster = null;
+    }
 }
+
---
+++ src/main/java/org/example/Dungeon.java
@@ -0,0 +1,111 @@
+package org.example;
+
+import java.util.*;
+
+/**
+ * The Dungeon class represents the entire dungeon game.
+ *
+ */
+public class Dungeon {
+    private Chamber currentChamber;
+    private Chamber endChamber;
+    private List<Chamber> chambers = new ArrayList<>();
+    private Player player;
+
+    /**
+     * Initializes a new dungeon with a player and sets up the dungeon chambers, doors,
+     * and items.
+     *
+     * @param player The player object representing the player in the game.
+     */
+    public Dungeon(Player player) {
+        this.player = player;
+        setupDungeon();
+    }
+
+    /**
+     * Sets up the dungeon by creating and connecting chambers, doors, and items.
+     */
+    private void setupDungeon() {
+        // Create chambers
+        Chamber start = new Chamber("Start");
+        Chamber interval = new Chamber("Interval");
+        Chamber end = new Chamber("End");
+
+        // Add doors (undirected)
+        Door door1 = new Door(start, interval, new Monster("Goblin", 5, 0, 10)); // Strength-based
+        Door door2 = new Door(interval, end, new Monster("Spider", 0, 5, 10)); // Craft-based
+
+        start.addDoor(door1);
+        interval.addDoor(door1);
+        interval.addDoor(door2);
+        end.addDoor(door2);
+
+        // Add items to chambers
+        start.addItem(new Sword("Iron Sword", 3));
+        interval.addItem(new Wand("Magic Wand", 4));
+
+        chambers.add(start);
+        chambers.add(interval);
+        chambers.add(end);
+
+        this.currentChamber = start;
+        this.endChamber = end;
+    }
+
+    /**
+     * @return True if the player is in the last chamber, false otherwise.
+     */
+    public boolean isFinished() {
+        return currentChamber.getName().equals("End");
+    }
+
+    /**
+     *
+     * @return The current Chamber object.
+     */
+    public Chamber getCurrentChamber() {
+        return currentChamber;
+    }
+
+    /**
+     *
+     * @param chamber The new Chamber object representing the player's location.
+     */
+    public void setCurrentChamber(Chamber chamber) {
+        this.currentChamber = chamber;
+    }
+
+    /**
+     * Returns the player object representing the player in the game.
+     *
+     */
+    public Character getPlayer() {
+        return player;
+    }
+
+    /**
+     *
+     * @return A list of Action objects representing the possible actions.
+     */
+    public List<Action> getActions() {
+        List<Action> actions = new ArrayList<>();
+
+        for (Item item : currentChamber.getItems()) {
+            actions.add(new Pick(player, currentChamber, item));
+        }
+
+        for (Door door : currentChamber.getDoors()) {
+            if (door.hasMonster()) {
+                actions.add(new Fight(player, door.getMonster()));
+            } else {
+                actions.add(new Move(this, door.getOtherChamber(currentChamber)));
+            }
+        }
+
+        return actions;
+    }
+}
+
+
+
